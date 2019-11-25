12075f7a0483ce206069c0588c23e507f2cabe0e
Deon Tan <deonjtan@gmail.com>
2019-11-25 00:12:48 +0000

    wip: scaffold google sheets

---
+++ src/clj/sheets_fn/google_sheets.clj
@@ -0,0 +1,69 @@
+(ns sheets-fn.google-sheets
+  (:require [google-apps-clj.google-sheets-v4 :as gs]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(def spreadsheet-id "1H-EpwDzmVlKR6W8gUO2sWOFrOci7zV7X04oLFBtNbEQ")
+(def model-sheet-id 0)
+(def inputs-sheet-id 1654169653)
+
+(def sheets-oauth-scope ["https://spreadsheets.google.com/feeds"])
+
+(comment
+  (require 'sheets-fn.google-sheets :reload)
+  (def credentials (edn/read-string (slurp "credentials.edn")))
+
+  (require '[google-apps-clj.credentials :as google.creds])
+
+  ;; (def gcreds (google.creds/default-credential))
+  ;; (def sheets-client
+  ;;   (gs/build-service gcreds))
+
+  (def oauth-map (google.creds/get-auth-map (select-keys (:oauth credentials)
+                                                         [:client-id
+                                                          :redirect-uris
+                                                          :client-secret])
+                                            sheets-oauth-scope))
+
+
+  ;; TODO look into service creds setup
+  (def sheets-client2
+    (gs/build-service (:oauth credentials)))
+
+
+  (str (java.util.UUID/randomUUID))
+  (def pp clojure.pprint/pprint)
+  (pp (gs/get-sheet-info sheets-client2 spreadsheet-id 0))
+  (pp (gs/get-cells sheets-client2 spreadsheet-id ["Inputs!A1:E9"]))
+  (gs/append-sheet sheets-client2 spreadsheet-id inputs-sheet-id [[(str (java.util.UUID/randomUUID))
+                                                                   "asldkfj"
+                                                                   "asldkfjaslkdfj"
+                                                                   "lorem"
+                                                                   "ajoiwe"
+                                                                   (gs/formula-cell (->function-cell))]])
+  )
+
+
+(defn ->input [col-idx]
+  (str "INDIRECT(ADDRESS(ROW()," col-idx "))"))
+
+(defn nth-char [s-cell n]
+  (str "RIGHTB(LEFTB(" s-cell "," n "),1)"))
+
+(defn ->function-cell []
+  (str "=CONCATENATE("
+    (str/join
+      ","
+      [(nth-char (->input 2) "Model!$A$2")
+       (nth-char (->input 3) "Model!$B$2")
+       (nth-char (->input 4) "Model!$C$2")
+       (nth-char (->input 5) "Model!$D$2")])
+    ")"))
+
+(defn append-inputs-to-sheet! [sheets-client input-1 input-2 input-3 input-4]
+  (let [job-id (str (java.util.UUID/randomUUID))
+        fn-cell (gs/formula-cell (->function-cell))]
+    (gs/append-sheet sheets-client
+                    spreadsheet-id
+                    inputs-sheet-id
+                    [[input-1 input-2 input-3 input-4 fn-cell]])))

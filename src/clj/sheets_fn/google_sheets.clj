5679e6bbb14cbfc0a6ab4b9c6b74d9cc1715cd4a
Deon Tan <deonjtan@gmail.com>
2019-11-25 14:24:16 +0000

    wip manually use java API for append and fetch updated result

--- src/clj/sheets_fn/google_sheets.clj
+++ src/clj/sheets_fn/google_sheets.clj
@@ -1,13 +1,58 @@
 (ns sheets-fn.google-sheets
   (:require [google-apps-clj.google-sheets-v4 :as gs]
             [clojure.edn :as edn]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import (com.google.api.services.sheets.v4.model ValueRange)))
 
 (def spreadsheet-id "1H-EpwDzmVlKR6W8gUO2sWOFrOci7zV7X04oLFBtNbEQ")
 (def model-sheet-id 0)
 (def inputs-sheet-id 1654169653)
 
 (def sheets-oauth-scope ["https://spreadsheets.google.com/feeds"])
+
+(defn ->input [col-idx]
+  (str "INDIRECT(ADDRESS(ROW()," col-idx "))"))
+
+(defn nth-char [s-cell n]
+  (str "RIGHTB(LEFTB(" s-cell "," n "),1)"))
+
+(defn ->function-cell []
+  (str "=CONCATENATE("
+    (str/join
+      ","
+      [(nth-char (->input 2) "Model!$A$2")
+       (nth-char (->input 3) "Model!$B$2")
+       (nth-char (->input 4) "Model!$C$2")
+       (nth-char (->input 5) "Model!$D$2")])
+    ")"))
+
+(defn get-recommendations [sheets-client input-1 input-2 input-3 input-4]
+  (let [job-id (str (java.util.UUID/randomUUID))
+        fn-cell (->function-cell)]
+    (let [append-resp (append-sheet sheets-client spreadsheet-id [[job-id input-1 input-2 input-3 input-4 fn-cell]])]
+      (let [updated-range (-> append-resp (get-in ["updates" "updatedRange"]))]
+        (let [updated-cell-values-resp (gs/get-cell-values sheets-client spreadsheet-id [updated-range])]
+          (let [[updated-job-id input-1* input-2* input-3* input-4* output] (first (first updated-cell-values-resp))]
+            (assert (= 1 (count updated-cell-values-resp)) "Should only update one row")
+            (assert (= job-id updated-job-id) (str "job-id should correspond to updated-job-id" job-id "," updated-job-id))
+            output))))))
+
+(defn append-sheet
+  [service spreadsheet-id rows]
+  (assert (not-empty rows) "Must write at least one row to the sheet")
+  (let [value-range (doto (ValueRange.)
+                      (.setValues rows))]
+    (-> (doto
+          ;; lol
+          (-> service
+              (.spreadsheets)
+              (.values)
+              (.append spreadsheet-id "Inputs!A:E" value-range))
+
+          (.set "includeValuesInResponse" true)
+          (.setValueInputOption "USER_ENTERED")
+          (.setInsertDataOption "INSERT_ROWS"))
+        (.execute))))
 
 (comment
   (require 'sheets-fn.google-sheets :reload)
@@ -42,28 +87,12 @@
                                                                    "ajoiwe"
                                                                    (gs/formula-cell (->function-cell))]])
   )
+(comment
+  (append-inputs-to-sheet! sheets-client2 "netflix" "google" "amazon" "facebook")
+  (get-recommendations sheets-client2 "netflix" "google" "amazon" "facebook")
+  ;; =>({"replies" [{}], "spreadsheetId" "1H-EpwDzmVlKR6W8gUO2sWOFrOci7zV7X04oLFBtNbEQ"})
 
+  (append-sheet sheets-client2 spreadsheet-id
+                [[(str (java.util.UUID/randomUUID)) "netflix" "google" "amazon" "facebook" (->function-cell)]])
+  )
 
-(defn ->input [col-idx]
-  (str "INDIRECT(ADDRESS(ROW()," col-idx "))"))
-
-(defn nth-char [s-cell n]
-  (str "RIGHTB(LEFTB(" s-cell "," n "),1)"))
-
-(defn ->function-cell []
-  (str "=CONCATENATE("
-    (str/join
-      ","
-      [(nth-char (->input 2) "Model!$A$2")
-       (nth-char (->input 3) "Model!$B$2")
-       (nth-char (->input 4) "Model!$C$2")
-       (nth-char (->input 5) "Model!$D$2")])
-    ")"))
-
-(defn append-inputs-to-sheet! [sheets-client input-1 input-2 input-3 input-4]
-  (let [job-id (str (java.util.UUID/randomUUID))
-        fn-cell (gs/formula-cell (->function-cell))]
-    (gs/append-sheet sheets-client
-                    spreadsheet-id
-                    inputs-sheet-id
-                    [[input-1 input-2 input-3 input-4 fn-cell]])))
